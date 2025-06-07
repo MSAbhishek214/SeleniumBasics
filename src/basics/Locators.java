168428c2f465a7c8384c74b4bddf6dc7b7ca84f5
Abhishek Rotti <abhishek.rotti755@gmail.com>
2025-06-07 16:35:37 +0530

    Clean up Locators file

--- src/basics/Locators.java
+++ src/basics/Locators.java
@@ -4,9 +4,7 @@ import java.time.Duration;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.Test;
 
-@Test
 public class Locators {
 
 	public static void main(String[] args) throws InterruptedException {
